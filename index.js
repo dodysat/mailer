205b6a10d43c1cf8fd0074139b2dece6d979af74
Dody Satria <dody.satria13@gmail.com>
2022-02-02 19:48:59 +0700

    update exec function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-exports.helloPubSub = (event, context) => {
+exports.mailerExec = (event, context) => {
   var payload = JSON.parse(Buffer.from(event.data, 'base64').toString());
   console.log(payload);
 };
